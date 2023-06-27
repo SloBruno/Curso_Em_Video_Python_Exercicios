e7e87e01bba2ab7e3437e635e48617af1b492df9
Bruno Slominski <slominskibruno@gmail.com>
2023-06-27 14:46:03 -0300

    change

--- ex041.py
+++ ex041.py
@@ -2,9 +2,17 @@
 
 from datetime import date
 
-nascimento = int(input('Digite o ano de nascimento do Atleta:'))
+nascimento = int(input('Digite o ano de nascimento do atleta:'))
 atual = date.today().year
 idade = atual - nascimento
 
-
-print(idade)
+if idade <= 9:
+    print('Categoria Mirim.')
+elif 9 < idade <= 14:
+    print('Categoria Infantil.')
+elif 14 < idade <= 19:
+    print('Categoria Júnior.')
+elif 19 < idade <= 20:
+    print('Categoria Sênior.')
+else:
+    print('Categoria Master.')
