ef87191cf04929c98f4ebbf4b0d43661db3bc47c
bruno_slominski <slominskibruno@gmail.com>
2023-08-08 12:22:27 -0300

    exercícios sobre lista

--- ex089.py
+++ ex089.py
@@ -1,15 +1,20 @@
 alunos = []
+nomes = []
+notas = []
+contador = 0
 
 while True:
 
     nome = str(input('Nome do Aluno:'))
-    alunos.append(nome)
-    nota_um = float(input('Nota 1:'))
-    alunos.append(nota_um)
-    nota_dois = float(input('Nota 2:'))
-    alunos.append(nota_dois)
-
-
+    nomes.append(nome)
+    notaum = int(input('Nota 1:'))
+    notas.append(notaum)
+    notadois = int(input('Nota 2:'))
+    notas.append(notadois)
+    nomes.append(notas[:])
+    alunos.append(nomes[:])
+    nomes.clear()
+    notas.clear()
     continuar = str(input('Deseja Continuar? [s/n]:')).lower().strip()
 
     if continuar == 's':
@@ -17,4 +22,9 @@ while True:
     else:
         break
 
-print(alunos)
+# Média
+
+print('-=-' *20)
+print(f'N° NOME       MÉDIA')
+
+print(alunos[0, 1])
