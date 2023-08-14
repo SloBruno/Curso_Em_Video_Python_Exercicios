04b8230bb8f72729494cb75b1873ffc2a31c97d3
bruno_slominski <slominskibruno@gmail.com>
2023-08-14 09:41:16 -0300

    exercícios sobre lista

--- ex093.py
+++ ex093.py
@@ -3,30 +3,34 @@ gols = list()
 total = 0
 
 jogador['nome'] = str(input('Nome do Jogador:'))
-partidas = int(input('Quantas partidas ele jogou?'))
+partidas = int(input(f'Quantas Partidas {jogador["nome"]} jogou?'))
 
-for i in range (0, partidas):
-    gol = int(input('Quantos gols na partida {}?' .format(i)))
+for i in range(0, partidas):
+    gol = int(input(f'Quantos gols na partida {i+1}?'))
     gols.append(gol)
     total += gol
 
 jogador['gols'] = gols
 jogador['total'] = total
 
-print('-=-'*25)
+print('-=-'*30)
 
 print(jogador)
 
-print('-=-'*25)
+print('-=-'*30)
 
 for k, v in jogador.items():
     print(f'O campo {k} tem o valor {v}.')
 
-print('-=-'*25)
+print('-=-'*30)
 
 nome = jogador['nome']
-print(f'O jogador {nome} jogou {partidas} partidas.')
 
-for i in range(0, partidas):
-        print(f'     =>Na partida {i}, fez {gols[i]} gols.')
-print(f'Foi um total de {total} gols.')
+print(f'O jogador {nome} jogou {partidas} partidas')
+
+for n, m in enumerate(gols):
+    print(f'    =>Na partida {n}, fez {m} gols')
+print(f'Foi um total de {total} gols')
+
+
+
--- ex094.py
+++ ex094.py
@@ -1,9 +1,13 @@
 pessoas = list()
 pessoa = dict()
+mulheres = list()
+maiores = list()
 
 while True:
     pessoa['nome'] = str(input('Nome:'))
-    pessoa['sexo'] = str(input('Sexo [M/F]:'))
+    pessoa['sexo'] = str(input('Sexo [M/F]:')).upper().strip()
+    if pessoa['sexo'] == 'F':
+        mulheres.append(pessoa['nome'])
     pessoa['idade'] = int(input('Idade:'))
 
     pessoas.append(pessoa.copy())
@@ -22,10 +26,24 @@ quant = len(pessoas)
 soma_idade = 0
 
 for v in pessoas:
-    for i in v['idade']:
-        soma_idade += i
+    idade = v['idade']
+    soma_idade += idade
 
 media = soma_idade/quant
 
-print(f'O grupo tem {quant} pessoas.'
-      f'A média de idade é de {media} anos.')
+for v in pessoas:
+    idade = v['idade']
+    if idade > media:
+        maior = v['nome']
+        maiores.append(maior)
+        maior = v['idade']
+        maiores.append(maior)
+
+print(f'O grupo tem {quant} pessoas.\n'
+      f'A média de idade é de {media :.2f} anos.\n'
+      f'As mulheres são {mulheres}\n', end='')
+for v in maiores:
+    print(v, 'tem a idade acima da média com', end='')
+    print(v, 'anos de idade.')
+    # print(f'{v} está acima da média com {v+1} anos de idade')
+
