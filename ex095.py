0b6f8028404a90dc79b7dc3b79d751c4d519a3a4
bruno_slominski <slominskibruno@gmail.com>
2023-08-16 08:25:35 -0300

    exercícios sobre dicionario

--- ex095.py
+++ ex095.py
@@ -1,14 +1,14 @@
 jogadores = list()
-total = 0
 
 while True:
+    total = 0
     print('-' * 25)
     jogador = dict()
     gols = list()
-    jogador['nome'] = str(input('Nome do Jogador:'))
+    jogador['nome'] = input('Nome do Jogador:')
     partidas = int(input(f'Quantas Partidas {jogador["nome"]} jogou?'))
 
-    for i in range(0, partidas):
+    for i in range(partidas):
         gol = int(input(f'Quantos gols na partida {i+1}?'))
         gols.append(gol)
         total += gol
@@ -18,43 +18,33 @@ while True:
 
     jogadores.append(jogador.copy())
     jogador.clear()
-    continuar = str(input('Deseja Continuar? [S/N]:')).upper().strip()
+    continuar = input('Deseja Continuar? [S/N]:').upper().strip()
 
     if continuar == 'N':
         break
-    else:
-        continue
 
 
 print('-=-'*30)
 
-
 print('cod      nome     gols       total')
 
 for c, v in enumerate(jogadores):
-    nome = v.get('nome')
-    score = v.get('gols')
-    total = v.get('total')
+    nome = v['nome']
+    score = v['gols']
+    total = v['total']
     print(f'{c}     {nome}      {score}     {total}')
-    total = 0
 
 print('-' * 25)
 
-
 while True:
     levantamento = int(input('Mostrar dados de qual jogador? [999 para parar]:'))
-    if levantamento != 999:
+    if levantamento != 999 and levantamento in range(len(jogadores)):
         print(f"-- LEVANTAMENTO do jogador {jogadores[levantamento]['nome']}:")
         for c, i in enumerate(jogadores[levantamento]['gols']):
-            print(f'No jogo {c} fez {i} gols.')
-    else:
+            print(f'No jogo {c+1} fez {i} gols.')
+    elif levantamento == 999:
         break
+    else:
+        print('Jogador não existente')
 
 print('<Programa Finalizado>')
-
-
-
-
-
-
-
