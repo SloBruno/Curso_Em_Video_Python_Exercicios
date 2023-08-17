8e4b7ac1b067177c6d3f0805e3f9d51bdffa23ed
bruno_slominski <slominskibruno@gmail.com>
2023-08-17 12:12:49 -0300

    exercícios sobre dicionario

--- ex091.py
+++ ex091.py
@@ -1,18 +1,31 @@
 from random import randint
 from time import sleep
+from operator import itemgetter
 
 jogadores = dict()
-ranking = list()
+ranking = dict()
 
 jogadores['jodador_um'] = randint(1, 6)
 jogadores['jodador_dois'] = randint(1, 6)
 jogadores['jodador_tres'] = randint(1, 6)
 jogadores['jodador_quatro'] = randint(1, 6)
 
-print('-=-=-=-=-=Ranking-=-=-=-=-=')
+print('Valores Sorteados')
 
 for k, v in jogadores.items():
     sleep(0.7)
     print(f'O {k} tirou {v}')
+
+ranking = sorted(jogadores.items(), key=itemgetter(1), reverse=True)
+
+
+print('-=-=-=-=-=Ranking-=-=-=-=-=')
+
+for i, v in enumerate(ranking):
+    print(f'    {i+1}° Lugar: {v[0]} com o número {v[1]}')
+    sleep(1)
+
+
+
 
 
